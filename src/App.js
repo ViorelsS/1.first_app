104602a69a5c19b8a7d245f6254940e7346d873a
Viorel Strogoteanu <viorelss@icloud.com>
2022-03-08 15:38:42 +0100

    [END]

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Main from "./Components/Main";
+import Navbar from "./Components/Navbar";
+
+function App() {
+    return (
+        <div>
+            <Main />
+            <Navbar />
+        </div>
+    );
+}
+
+export default App;
