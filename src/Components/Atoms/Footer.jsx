30ef8ba3393194671515ee42e340ab6d6c77b1bc
Viorel Strogoteanu <viorelss@icloud.com>
2022-03-08 14:46:21 +0100

    [DEV]initialized the project

--- src/Components/Atoms/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-    return (
-        <div className="bg-gray-500">
-            <p className="text-xs">Small footer at the end of the page</p>
-        </div>
-    );
-}
-
-export default Footer;
